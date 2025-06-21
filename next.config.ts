37afd338a888e903857e80c19622266d38e20f90
Aman Dubey <amandubey8833@gmail.com>
2025-06-21 20:49:54 +0530

    Feat : UI Enhancments and project section

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
+  images: {
+    // ↓ pick ONE of these two options
+    /* 1. simple */
+    domains: ["images.unsplash.com", "amandubey.onrender.com"],
+
+    /* 2. fine-grained – if you prefer remotePatterns */
+    /* remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        pathname: '/**',     // accept any path
+      },
+    ], */
+  },
 };
 
 export default nextConfig;
