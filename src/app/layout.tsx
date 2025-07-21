d14299fc459ef5d9e954482244f9aec318a4fb14
Aman Dubey <amandubey8833@gmail.com>
2025-07-21 18:06:06 +0530

    Feat: Call To Action Reframed and hidded from Contact Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,9 @@
 import "@fontsource/inter";
 import "./globals.css";
 import { NavBar } from "@/components/NavBar";
-import LenisWrapper from "@/components/LenisWrapper";
-import { Footer } from "@/components/Footer";
-import { CallToActionSection } from "@/components/CallToActionSection";
 import { sora } from "@/components/fonts";
 import { Analytics } from "@vercel/analytics/next";
+import { ClientLayoutContent } from "./ClientLayoutContent";
 
 export default function RootLayout({
   children,
@@ -15,14 +13,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    // Add className="dark" to the html tag here
     <html lang="en" className="dark" suppressHydrationWarning>
       <body className={`${sora.className} antialiased`}>
         <NavBar />
         <main className="pt-28 md:pt-32">
-          <LenisWrapper>{children}</LenisWrapper>
-          <CallToActionSection />
-          <Footer />
+          <ClientLayoutContent>{children}</ClientLayoutContent>
         </main>
         <Analytics />
       </body>
